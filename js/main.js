8351258c54967d08aa739bf6c5123f869f8fe5d7
sonagurbanova3 <sonagurbanova3@gmail.com>
2022-08-31 17:57:56 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* ================= MOBILE NAV ========================= */
+const mobileNavButton = document.querySelector('.mobile-nav-button');
+const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
+const mobileNav = document.querySelector('.nav-menu');
+
+mobileNavButton.addEventListener('click', function () {
+	mobileNavIcon.classList.toggle('active');
+	mobileNav.classList.toggle('active');
+	document.body.classList.toggle('no-scroll');
+});
